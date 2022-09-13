2af44273824959eb1b2625b200c34cb382113c3d
IVIosab <mosab.f.r@gmail.com>
2022-09-13 18:06:43 +0300

    back to work

--- src/App.js
+++ src/App.js
@@ -20,203 +20,203 @@ class App extends React.Component {
           <div className='team-1 flex border w-2/3 m-1'> 
             <div className='player-1 flex flex-col w-1/6 items-center'>
               <img src={images['RGE']['Top']}/>
-              <p className='text-white text-4xl'>TOP</p>
+              <p className='text-white text-4xl'>Top</p>
             </div>
             <div className='player-2 flex flex-col w-1/6 items-center'>
             <img src={images['RGE']['Jung']}/>
-              <p className='text-white text-4xl'>JGL</p>
+              <p className='text-white text-4xl'>Jung</p>
             </div>
             <div className='player-3 flex flex-col w-1/6 items-center'>
-              <p className='text-white text-4xl'>MID</p>
+              <p className='text-white text-4xl'>Mid</p>
             </div>
             <div className='player-4 flex flex-col w-1/6 items-center'>
               <p className='text-white text-4xl'>ADC</p>
             </div>
             <div className='player-5 flex flex-col w-1/6 items-center'>
-              <p className='text-white text-4xl'>SUP</p>
+              <p className='text-white text-4xl'>Supp</p>
             </div>
             <div className='player-6 flex flex-col w-1/6 items-center'>
-              <p className='text-white text-4xl'>CCH</p>
+              <p className='text-white text-4xl'>Coach</p>
             </div>
           </div>
           <div className='team-2 flex border w-2/3 m-1'>
             <div className='player-1 flex flex-col w-1/6 items-center'>
-              <p className='text-white text-4xl'>TOP</p>
+              <p className='text-white text-4xl'>Top</p>
             </div>
             <div className='player-2 flex flex-col w-1/6 items-center'>
-              <p className='text-white text-4xl'>JGL</p>
+              <p className='text-white text-4xl'>Jung</p>
             </div>
             <div className='player-3 flex flex-col w-1/6 items-center'>
-              <p className='text-white text-4xl'>MID</p>
+              <p className='text-white text-4xl'>Mid</p>
             </div>
             <div className='player-4 flex flex-col w-1/6 items-center'>
               <p className='text-white text-4xl'>ADC</p>
             </div>
             <div className='player-5 flex flex-col w-1/6 items-center'>
-              <p className='text-white text-4xl'>SUP</p>
+              <p className='text-white text-4xl'>Supp</p>
             </div>
             <div className='player-6 flex flex-col w-1/6 items-center'>
-              <p className='text-white text-4xl'>CCH</p>
+              <p className='text-white text-4xl'>Coach</p>
             </div>
           </div>
           <div className='team-3 flex border w-2/3 m-1'>
             <div className='player-1 flex flex-col w-1/6 items-center'>
-              <p className='text-white text-4xl'>TOP</p>
+              <p className='text-white text-4xl'>Top</p>
             </div>
             <div className='player-2 flex flex-col w-1/6 items-center'>
-              <p className='text-white text-4xl'>JGL</p>
+              <p className='text-white text-4xl'>Jung</p>
             </div>
             <div className='player-3 flex flex-col w-1/6 items-center'>
-              <p className='text-white text-4xl'>MID</p>
+              <p className='text-white text-4xl'>Mid</p>
             </div>
             <div className='player-4 flex flex-col w-1/6 items-center'>
               <p className='text-white text-4xl'>ADC</p>
             </div>
             <div className='player-5 flex flex-col w-1/6 items-center'>
-              <p className='text-white text-4xl'>SUP</p>
+              <p className='text-white text-4xl'>Supp</p>
             </div>
             <div className='player-6 flex flex-col w-1/6 items-center'>
-              <p className='text-white text-4xl'>CCH</p>
+              <p className='text-white text-4xl'>Coach</p>
             </div>
           </div>
           <div className='team-4 flex border w-2/3 m-1'>
             <div className='player-1 flex flex-col w-1/6 items-center'>
-              <p className='text-white text-4xl'>TOP</p>
+              <p className='text-white text-4xl'>Top</p>
             </div>
             <div className='player-2 flex flex-col w-1/6 items-center'>
-              <p className='text-white text-4xl'>JGL</p>
+              <p className='text-white text-4xl'>Jung</p>
             </div>
             <div className='player-3 flex flex-col w-1/6 items-center'>
-              <p className='text-white text-4xl'>MID</p>
+              <p className='text-white text-4xl'>Mid</p>
             </div>
             <div className='player-4 flex flex-col w-1/6 items-center'>
               <p className='text-white text-4xl'>ADC</p>
             </div>
             <div className='player-5 flex flex-col w-1/6 items-center'>
-              <p className='text-white text-4xl'>SUP</p>
+              <p className='text-white text-4xl'>Supp</p>
             </div>
             <div className='player-6 flex flex-col w-1/6 items-center'>
-              <p className='text-white text-4xl'>CCH</p>
+              <p className='text-white text-4xl'>Coach</p>
             </div>
           </div>
           <div className='team-5 flex border w-2/3 m-1'>
             <div className='player-1 flex flex-col w-1/6 items-center'>
-              <p className='text-white text-4xl'>TOP</p>
+              <p className='text-white text-4xl'>Top</p>
             </div>
             <div className='player-2 flex flex-col w-1/6 items-center'>
-              <p className='text-white text-4xl'>JGL</p>
+              <p className='text-white text-4xl'>Jung</p>
             </div>
             <div className='player-3 flex flex-col w-1/6 items-center'>
-              <p className='text-white text-4xl'>MID</p>
+              <p className='text-white text-4xl'>Mid</p>
             </div>
             <div className='player-4 flex flex-col w-1/6 items-center'>
               <p className='text-white text-4xl'>ADC</p>
             </div>
             <div className='player-5 flex flex-col w-1/6 items-center'>
-              <p className='text-white text-4xl'>SUP</p>
+              <p className='text-white text-4xl'>Supp</p>
             </div>
             <div className='player-6 flex flex-col w-1/6 items-center'>
-              <p className='text-white text-4xl'>CCH</p>
+              <p className='text-white text-4xl'>Coach</p>
             </div>
           </div>
           <div className='team-6 flex border w-2/3 m-1'>
             <div className='player-1 flex flex-col w-1/6 items-center'>
-              <p className='text-white text-4xl'>TOP</p>
+              <p className='text-white text-4xl'>Top</p>
             </div>
             <div className='player-2 flex flex-col w-1/6 items-center'>
-              <p className='text-white text-4xl'>JGL</p>
+              <p className='text-white text-4xl'>Jung</p>
             </div>
             <div className='player-3 flex flex-col w-1/6 items-center'>
-              <p className='text-white text-4xl'>MID</p>
+              <p className='text-white text-4xl'>Mid</p>
             </div>
             <div className='player-4 flex flex-col w-1/6 items-center'>
               <p className='text-white text-4xl'>ADC</p>
             </div>
             <div className='player-5 flex flex-col w-1/6 items-center'>
-              <p className='text-white text-4xl'>SUP</p>
+              <p className='text-white text-4xl'>Supp</p>
             </div>
             <div className='player-6 flex flex-col w-1/6 items-center'>
-              <p className='text-white text-4xl'>CCH</p>
+              <p className='text-white text-4xl'>Coach</p>
             </div>
           </div>
           <div className='team-7 flex border w-2/3 m-1'>
             <div className='player-1 flex flex-col w-1/6 items-center'>
-              <p className='text-white text-4xl'>TOP</p>
+              <p className='text-white text-4xl'>Top</p>
             </div>
             <div className='player-2 flex flex-col w-1/6 items-center'>
-              <p className='text-white text-4xl'>JGL</p>
+              <p className='text-white text-4xl'>Jung</p>
             </div>
             <div className='player-3 flex flex-col w-1/6 items-center'>
-              <p className='text-white text-4xl'>MID</p>
+              <p className='text-white text-4xl'>Mid</p>
             </div>
             <div className='player-4 flex flex-col w-1/6 items-center'>
               <p className='text-white text-4xl'>ADC</p>
             </div>
             <div className='player-5 flex flex-col w-1/6 items-center'>
-              <p className='text-white text-4xl'>SUP</p>
+              <p className='text-white text-4xl'>Supp</p>
             </div>
             <div className='player-6 flex flex-col w-1/6 items-center'>
-              <p className='text-white text-4xl'>CCH</p>
+              <p className='text-white text-4xl'>Coach</p>
             </div>
           </div>
           <div className='team-8 flex border w-2/3 m-1'>
             <div className='player-1 flex flex-col w-1/6 items-center'>
-              <p className='text-white text-4xl'>TOP</p>
+              <p className='text-white text-4xl'>Top</p>
             </div>
             <div className='player-2 flex flex-col w-1/6 items-center'>
-              <p className='text-white text-4xl'>JGL</p>
+              <p className='text-white text-4xl'>Jung</p>
             </div>
             <div className='player-3 flex flex-col w-1/6 items-center'>
-              <p className='text-white text-4xl'>MID</p>
+              <p className='text-white text-4xl'>Mid</p>
             </div>
             <div className='player-4 flex flex-col w-1/6 items-center'>
               <p className='text-white text-4xl'>ADC</p>
             </div>
             <div className='player-5 flex flex-col w-1/6 items-center'>
-              <p className='text-white text-4xl'>SUP</p>
+              <p className='text-white text-4xl'>Supp</p>
             </div>
             <div className='player-6 flex flex-col w-1/6 items-center'>
-              <p className='text-white text-4xl'>CCH</p>
+              <p className='text-white text-4xl'>Coach</p>
             </div>
           </div>
           <div className='team-9 flex border w-2/3 m-1'>
             <div className='player-1 flex flex-col w-1/6 items-center'>
-              <p className='text-white text-4xl'>TOP</p>
+              <p className='text-white text-4xl'>Top</p>
             </div>
             <div className='player-2 flex flex-col w-1/6 items-center'>
-              <p className='text-white text-4xl'>JGL</p>
+              <p className='text-white text-4xl'>Jung</p>
             </div>
             <div className='player-3 flex flex-col w-1/6 items-center'>
-              <p className='text-white text-4xl'>MID</p>
+              <p className='text-white text-4xl'>Mid</p>
             </div>
             <div className='player-4 flex flex-col w-1/6 items-center'>
               <p className='text-white text-4xl'>ADC</p>
             </div>
             <div className='player-5 flex flex-col w-1/6 items-center'>
-              <p className='text-white text-4xl'>SUP</p>
+              <p className='text-white text-4xl'>Supp</p>
             </div>
             <div className='player-6 flex flex-col w-1/6 items-center'>
-              <p className='text-white text-4xl'>CCH</p>
+              <p className='text-white text-4xl'>Coach</p>
             </div>
           </div>
           <div className='team-10 flex border w-2/3 m-1'>
             <div className='player-1 flex flex-col w-1/6 items-center'>
-              <p className='text-white text-4xl'>TOP</p>
+              <p className='text-white text-4xl'>Top</p>
             </div>
             <div className='player-2 flex flex-col w-1/6 items-center'>
-              <p className='text-white text-4xl'>JGL</p>
+              <p className='text-white text-4xl'>Jung</p>
             </div>
             <div className='player-3 flex flex-col w-1/6 items-center'>
-              <p className='text-white text-4xl'>MID</p>
+              <p className='text-white text-4xl'>Mid</p>
             </div>
             <div className='player-4 flex flex-col w-1/6 items-center'>
               <p className='text-white text-4xl'>ADC</p>
             </div>
             <div className='player-5 flex flex-col w-1/6 items-center'>
-              <p className='text-white text-4xl'>SUP</p>
+              <p className='text-white text-4xl'>Supp</p>
             </div>
             <div className='player-6 flex flex-col w-1/6 items-center'>
-              <p className='text-white text-4xl'>CCH</p>
+              <p className='text-white text-4xl'>Coach</p>
             </div>
           </div>
         </div>
